b349883e78b813ff74c3be0c019e83281c5910f5
mostafa <info@alborzvoip.ir>
2025-12-14 05:35:25 +0330

    final-commit

--- obj/Debug/net9.0-windows/FileToText-WPF.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/FileToText-WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileToText-WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2bfa6e96951ec79e6d47e33860dd4041378deb")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileToText-WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileToText-WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
